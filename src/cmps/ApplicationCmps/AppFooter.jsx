92d4c92292fc0a9231b21999a2b235d729b5bac6
Lior Lazar <82064378+V1Su4L@users.noreply.github.com>
2025-11-25 23:04:30 +0200

    Improve footer

--- src/cmps/ApplicationCmps/AppFooter.jsx
+++ src/cmps/ApplicationCmps/AppFooter.jsx
@@ -2,7 +2,7 @@ export function AppFooter() {
     return (
         <footer className="app-footer">
             <p className="footer-text">
-                © 2025 PrimeDev — Full Stack & DevOps Engineering
+                © 2025 PrimeDev — פתרונות Full-Stack, DevOps ו-SOAR
             </p>
         </footer>
     )
